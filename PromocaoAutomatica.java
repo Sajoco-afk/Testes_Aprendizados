6408b80b7fc60706390f7bb66f7b900024f3bf62
Samuel Covalski <samuelcovalski@gmail.com>
2026-09-06 21:35:25 -0300

    Promocao1.0
     Teste de promoções automáticas.

---
+++ PromocaoAutomatica.java
@@ -0,0 +1,20 @@
+public class PromocaoAutomatica{
+	public static void main(String[] args) {
+	
+	byte a = 10;
+	short b = 20;
+	char c = 'A';
+	int d = 30;
+	long e = 40;
+	float f = 1.5f;
+	double g = 2.5;
+
+	System.out.println("a * b = " + a * b);
+	System.out.println("a + g = " + (a + g));
+	System.out.println("b - b = " + (b - b));
+	System.out.println("f / b = " + f / b);
+	System.out.println("g * c = " + g * c);
+	System.out.println("d * b = " + d * b);
+
+	}
+}
